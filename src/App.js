4cbeb1501f1f103604974a6f6b40a98ae6c7e274
Thanakit Charoenboonvivat <54524621+m1crochip@users.noreply.github.com>
2022-06-06 15:43:08 +0700

    First Create

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  AppBar,
+  Container,
+  Grid,
+  Grow,
+  Typography,
+} from "@material-ui/core";
+import memories from "./images/instal.png";
+import Posts from "./components/Posts";
+import Form from "./components/Form";
+import useStyles from "./styles";
+
+const App = () => {
+  const classes = useStyles();
+  return (
+    <Container maxWidth="lg">
+      <AppBar
+        className={classes.appBar}
+        position="static"
+        color="inherit"
+      >
+        <Typography
+          className={classes.heading}
+          variant="h1"
+          align="center"
+        >
+          Memories
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          heigth="61"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
+
+export default App;
